cdf441718e2a7b2256ea476de79fc2a7cba9ead9
zhan <zhantewei@gmail.com>
2024-06-12 17:50:48 +0800

    修复引入bug

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,7 +2,7 @@ import emailValidator from "./validators/email.validator";
 import maxValidator from "./validators/max.validator";
 import maxLengthValidator from "./validators/maxLength.validator";
 import minValidator from "./validators/min.validator";
-import minLengthValidator from "./validators/min.validator";
+import minLengthValidator from "./validators/minLength.validator";
 import rangeValidator from "./validators/range.validator";
 import rangeLengthValidator from "./validators/rangeLength.validator";
 import requiredValidator from "./validators/required.validator";
